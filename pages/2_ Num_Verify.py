843a7a053035d5993a6c7688d477deac69eb3b84
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-09-07 12:04:46 +0530

    Update 2_ Num_Verify.py

--- pages/2_ Num_Verify.py
+++ pages/2_ Num_Verify.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from googlesearch import search
 import pandas as pd
 import bz2file as bz2
+import re
 
 sl.set_page_config(page_title='Mobile Spam Detection', page_icon = "hacker.png")
 
@@ -65,172 +66,113 @@ sl.markdown(f"{style}",unsafe_allow_html=True)
 
 title = 'Visecure'
 
+
+sl.markdown("""
+<style>
+/* Apply styles to the "section-basic" class */
+.section-basic {
+    padding: 20px;
+}
+
+.h1-subline {
+    font-size: 18px;
+    color: #ff9933;
+}
+
+.well {
+    border: 1px solid ;
+    border-radius: 15px;
+    padding: 15px;
+}
+
+.container{
+    padding: 20px;
+    border-radius: 15px;
+    border: 1px solid;
+}
+
+/* Apply styles to the "zusammenfassung-serioesitaet" class */
+.zusammenfassung-serioesitaet {
+    background-color: #4caf50; /* Green background color */
+    color: #fff; /* White text color */
+}
+
+/* Add any additional styles you want for other elements with their respective classes */
+
+</style>
+""", unsafe_allow_html=True)
+
 sl.markdown(f"<h1 style='text-align: center;color:rgb(50,121,123);'>{title}</h1>", unsafe_allow_html=True)
 
-# def is_genuine_number(number):
-#     if number.startswith('9') or number.startswith('8') or number.startswith('7') or number.startswith('6') and len(number) == 10:  # Including the '91' prefix
-#         return True
-#     elif number.startswith('144') and (len(number) != 10):
-#         return False
-#     else:
-#         return False
+def num(number):
+    url = f"https://spamcalls.net/en/search?q={number}"
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return "Error"
 
-# def has_unusual_area_code(number):
-#     area_codes = ['001', '002', '123', '456', '999']  # Example of unusual area codes
-#     prefix = number[:3]
-#     return prefix in area_codes
-
-# def has_repeating_digits(number):
-#     for i in range(len(number) - 2):
-#         if number[i] == number[i+1] == number[i+2]:
-#             return True
-#     return False
-
-# def has_random_digit_sequence(number):
-#     consecutive_count = 0
-#     for i in range(1, len(number)):
-#         if abs(int(number[i]) - int(number[i-1])) <= 1:
-#             consecutive_count += 1
-#         else:
-#             consecutive_count = 0
-#         if consecutive_count >= 3:
-#             return True
-#     return False
-
-
-import requests
-def is_genuine_number(number):
-    url = f"https://truecaller-data1.p.rapidapi.com/rapid_api/truecaller/{number}"
-
-    headers = {
-        "X-RapidAPI-Key": "fcdf6023f9mshc7cf048dfe8bfe4p1432adjsn1633aeb1bf4e", #fcdf6023f9mshc7cf048dfe8bfe4p1432adjsn1633aeb1bf4e
-        "X-RapidAPI-Host": "truecaller-data1.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    # response = {
-    #     "success": True,
-    #     "data": [
-    #         {
-    #         "id": "jWAp5q77Jk/eN5OIwkiZ7g==",
-    #         "name": "Dedi Suwandi Yagami",
-    #         "imId": "1i9tibozg36kg",
-    #         "gender": "UNKNOWN",
-    #         "about": "",
-    #         "jobTitle": "",
-    #         "score": 0.9,
-    #         "access": "PUBLIC",
-    #         "enhanced": True,
-    #         "companyName": "",
-    #         "phones": [
-    #             {
-    #             "e164Format": "+6281223222224",
-    #             "numberType": "MOBILE",
-    #             "nationalFormat": "0812-2322-2224",
-    #             "dialingCode": 62,
-    #             "countryCode": "ID",
-    #             "carrier": "Telkomsel",
-    #             "type": "openPhone"
-    #             }
-    #         ],
-    #         "addresses": [
-    #             {
-    #             "address": "ID",
-    #             "street": "",
-    #             "zipCode": "",
-    #             "countryCode": "ID",
-    #             "timeZone": "+07:00",
-    #             "type": "address"
-    #             }
-    #         ],
-    #         "internetAddresses": [],
-    #         "badges": [
-    #             "premium",
-    #             "user"
-    #         ],
-    #         "tags": [],
-    #         "cacheTtl": 1296000000,
-    #         "sources": [],
-    #         "searchWarnings": [],
-    #         "surveys": [
-    #             {
-    #             "id": "0905a705-3c8c-48ab-a4a6-001ce0b20566",
-    #             "frequency": 86400,
-    #             "passthroughData": "eyAiMyI6ICI2MjgxMjIzMjIyMjI0IiwgIjIiOiAiRGVkaSBTdXdhbmRpIFlhZ2FtaSIsICI0IjogInBmIiB9",
-    #             "perNumberCooldown": 31536000
-    #             },
-    #             {
-    #             "id": "4eb9afcf-f36e-430d-86f9-4f72ca091f91",
-    #             "frequency": 86400,
-    #             "passthroughData": "eyAiMyI6ICI2MjgxMjIzMjIyMjI0IiwgIjIiOiAiRGVkaSBTdXdhbmRpIFlhZ2FtaSIsICI0IjogInBmIiB9",
-    #             "perNumberCooldown": 31536000
-    #             }
-    #         ],
-    #         "commentsStats": {
-    #             "showComments": False
-    #         },
-    #         "ns": 0
-    #         }
-    #     ]
-    # }
-    # if response['success']:
-    #     #sl.write(response['data'])
-    return response
 
 def main():
-    sl.title("Spam Call Verification")
-    number = sl.text_input("Enter your 10 digit number")  # Input number here
-    
-
-
-    def display(response):
-        sl.markdown(f"""
-            ## Details
-            <div style="color:#ffffff;padding:20px;border: 1px solid; border-radius:15px;margin:15px;">
-                <strong>Name :</strong> {response['data'][0]['name']}<br>
-                <strong>Phone Number :</strong> {response['data'][0]['phones'][0]['e164Format']}<br>
-                <strong>Carrier :</strong> {response['data'][0]['phones'][0]['carrier']}<br>
-                <strong>Country :</strong> {response['data'][0]['phones'][0]['countryCode']}<br>
-                <strong>Time Zone :</strong> {response['data'][0]['addresses'][0]['timeZone']}<br>
-                <strong>Score :</strong> {response['data'][0]['score']}<br>
-                <strong>Badges :</strong> {response['data'][0]['badges']}<br>
-                <strong>Tags :</strong> {response['data'][0]['tags']}<br>
-                <strong>Cache Ttl :</strong> {response['data'][0]['cacheTtl']}<br>
-                <strong>Sources :</strong> {response['data'][0]['sources']}<br>
-                <strong>Search Warnings :</strong> {response['data'][0]['searchWarnings']}<br>
-                <strong>Surveys :</strong><br>
-                <ul>
-                    <li> <strong>id :</strong> {response['data'][0]['surveys'][0]['id']}<br> </li>
-                    <li> <strong>frequency :</strong> {response['data'][0]['surveys'][0]['frequency']}<br> </li>
-                    <li> <strong>passthroughData :</strong> {response['data'][0]['surveys'][0]['passthroughData']}<br> </li>
-                    <li> <strong>perNumberCooldown :</strong> {response['data'][0]['surveys'][0]['perNumberCooldown']}<br> </li>
-                    <li> <strong>id :</strong> {response['data'][0]['surveys'][1]['id']}<br> </li>
-                    <li> <strong>frequency :</strong> {response['data'][0]['surveys'][1]['frequency']}<br> </li>
-                    <li> <strong>passthroughData :</strong> {response['data'][0]['surveys'][1]['passthroughData']}<br> </li>
-                </ul>
-                <strong>Comments Stats :</strong> {response['data'][0]['commentsStats']}<br>
-                <strong>Ns :</strong> {response['data'][0]['ns']}<br>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    if sl.button('Verify'):
-        if len(number) == 10:
-            response = is_genuine_number("+91"+number)
-            if response['success']:
-                if response['success']:
-                    sl.success("Verified Number")
-                    display(response)
-                    
-                else:
-                    sl.error("Unverified Number")
-                    display(response)
-            else:
-                sl.error("Server Down")
+    sl.title("Number Verification")
+    sl.markdown("""
+    <div id="rating">
+    </div>
+    """,unsafe_allow_html=True)
+    number = sl.text_input("Enter the mobile number")
+    if sl.button("Verify"):
+        report = num(number)
+        if report == "Error":
+            sl.error("Error occured")
         else:
-            sl.error("Invalid Number")
+            pattern = r'<div class="main main-raised">(.+?)</div>'
+
+            # Use re.search to find the first match
+            match = re.search(pattern, report, re.DOTALL)
+
+            # Extract the matched content
+            if match:
+                extracted_content = match.group(1)
+                sl.header(":blue[Report]")
+                modified_content = re.sub(r'<a href="#ratings">', '', extracted_content)
+                output_string = re.sub(r'<i[^>]*>.*?</i>', '', modified_content)
+                sl.markdown(f"{output_string}", unsafe_allow_html=True)
+                if "Medium" in extracted_content:
+                    sl.markdown("""
+                    <style>                  
+                    .well {
+                        border: 1px solid ;
+                        border-radius: 15px;
+                        padding: 15px;
+                        background-color: #ff9933;
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)
+                if "High" in extracted_content:
+                    sl.markdown("""
+                    <style>
+                    .well {
+                        border: 1px solid ;
+                        border-radius: 15px;
+                        padding: 15px;
+                        background-color: rgba(255,0,0,0.7);
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)
+                if "Low" in extracted_content:
+                    sl.markdown("""
+                    <style>
+                    .well {
+                        border: 1px solid ;
+                        border-radius: 15px;
+                        padding: 15px;
+                        background-color: rgba(76,175,80,0.7);
+                    }
+
+                    </style>
+                    """, unsafe_allow_html=True)
+
+
 if __name__ == "__main__":
     main()
 
