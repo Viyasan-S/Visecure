9dbca05ef2302141e75b6de0b157580775ce5deb
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-09-07 12:05:15 +0530

    Update 5_File_Scanner.py

--- pages/5_File_Scanner.py
+++ pages/5_File_Scanner.py
@@ -1,63 +1,27 @@
-# import virustotal_python
-
-# with virustotal_python.Virustotal("aa7529d5e8ec55dd973a5a9fd1e9a35cb9b16a4ee10d702ad4220c5cb8018896") as vtotal:
-
-import vt
 import streamlit as st 
-from streamlit_option_menu import option_menu
-import time
-cl = vt.Client("aa7529d5e8ec55dd973a5a9fd1e9a35cb9b16a4ee10d702ad4220c5cb8018896")
 
 st.title(":blue[File] Scanner")
 
-uploaded_file = st.file_uploader("Upload a file")
+detection = False
 
-if st.button("Scan File"):
-    if uploaded_file is not None:
-        # Upload the file and get the file ID
-        fileid = cl.scan_file(uploaded_file)
+malicious = ["rd c:\system32", "del c:\\", "rd c:\\"]
 
-        st.write("Scanning file...")
-        while True:
-            # Get the analysis status
-            analysis = cl.get_object("/analyses/{}", fileid.id)
-            st.write(f"Status: {analysis.status}")
-            if analysis.status == "completed":
-                break
-            time.sleep(5)
+fs = st.file_uploader("Upload a file")
 
-        try:
-            # Fetch the scan report
-            report = cl.get_object("/files/{}".format(fileid.id))
-        
-            # Display the report
-            st.write("Scan Report:")
-            st.markdown(
-                f'''
-                <style>
-                    .container {{
-                        border: 1px solid #ccc;
-                        border-radius: 10px;
-                        padding: 10px;
-                        margin: 10px;
-
-                    }}
-                </style>
-                <div class="container">
-                    <b>File Name:</b> {uploaded_file.name}<br>
-                    <b>File Size:</b> {report.size}<br>
-                    <b>File MD5:</b> {report.md5}<br>
-                </div>
-                ''',
-                unsafe_allow_html=True
-            )
-            if report.last_analysis_stats['malicious'] > 0:
-                st.warning("This file is malicious!")
-            else:
-                st.success("This file is not malicious.")
-                
-        except vt.APIError as e:
-            st.error(f"Error retrieving report: {e}")
-        
-        # Close the VirusTotal Client
-        cl.close()
+if st.button("Scan"):
+    if fs is not None:
+        if fs.name.endswith(".bat"):
+            batch = fs.getvalue().decode("utf-8")
+            codes = batch.split("\n")
+            for line in codes:
+                for maliciouscode in malicious:
+                    if maliciouscode.lower() == line.lower():
+                        detection = True
+                if detection == True:
+                    st.error("Virus detected!")
+                else:
+                    st.success("No virus detected!")
+        else:
+            st.success("No virus detected!")
+    else:
+        st.error("Please upload a file!")
